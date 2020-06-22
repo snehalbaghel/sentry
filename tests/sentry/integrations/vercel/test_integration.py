ac0100b9a15b88f95b3376de0905af1cb2235ec7
Colleen O'Rourke <colleen@sentry.io>
2020-06-22 14:16:51 -0700

    prelim work on tests

--- tests/sentry/integrations/vercel/test_integration.py
+++ tests/sentry/integrations/vercel/test_integration.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 import responses
 
 from six.moves.urllib.parse import parse_qs, urlencode
-
+from sentry.shared_integrations.exceptions import ApiError
 from sentry.integrations.vercel import VercelIntegrationProvider
 from sentry.models import (
     Integration,
@@ -94,3 +94,111 @@ class VercelIntegrationTest(IntegrationTestCase):
     @responses.activate
     def test_user_flow(self):
         self.assert_setup_flow(is_team=False)
+
+    @responses.activate
+    def test_update_organization_config(self):
+        with self.tasks():
+            self.assert_setup_flow()
+
+        # mock org secret
+        responses.add(
+            responses.GET,
+            "https://api.vercel.com/v3/now/secrets/%s" % "sentry_org",
+            body=ApiError('The secret "%s" was not found.' % "sentry_org"),
+            status=404,
+        )
+        responses.add(
+            responses.POST, "https://api.vercel.com/v2/now/secrets", json={"uid": "sec_123"},
+        )
+
+        # mock project secret
+        responses.add(
+            responses.GET,
+            "https://api.vercel.com/v3/now/secrets/%s" % "sentry_project_1",
+            body=ApiError('The secret "%s" was not found.' % "sentry_project_1"),
+            status=404,
+        )
+        responses.add(
+            responses.POST, "https://api.vercel.com/v2/now/secrets", json={"uid": "sec_456"},
+        )
+        # mock DSN secret
+        responses.add(
+            responses.GET,
+            "https://api.vercel.com/v3/now/secrets/%s" % "next_sentry_dsn_1",
+            body=ApiError('The secret "%s" was not found.' % "next_sentry_dsn_1"),
+            status=404,
+        )
+        responses.add(
+            responses.POST, "https://api.vercel.com/v2/now/secrets", json={"uid": "sec_789"},
+        )
+
+        # mock org env var
+        responses.add(
+            responses.POST,
+            "https://api.vercel.com/v4/projects/%s/env"
+            % "Qme9NXBpguaRxcXssZ1NWHVaM98MAL6PHDXUs1jPrgiM8H",
+            json={
+                "value": "sec_123",
+                "target": "production",
+                "configurationId": 1,
+                "key": "blah",
+                "createdAt": 1592849410863,
+                "updatedAt": 1592849410863,
+                "system": False,
+            },
+        )
+        # mock project env var
+        responses.add(
+            responses.POST,
+            "https://api.vercel.com/v4/projects/%s/env"
+            % "Qme9NXBpguaRxcXssZ1NWHVaM98MAL6PHDXUs1jPrgiM8H",
+            json={
+                "value": "sec_456",
+                "target": "production",
+                "configurationId": 1,
+                "key": "blah",
+                "createdAt": 1592849410863,
+                "updatedAt": 1592849410863,
+                "system": False,
+            },
+        )
+        # mock dsn env var
+        responses.add(
+            responses.POST,
+            "https://api.vercel.com/v4/projects/%s/env"
+            % "Qme9NXBpguaRxcXssZ1NWHVaM98MAL6PHDXUs1jPrgiM8H",
+            json={
+                "value": "sec_789",
+                "target": "production",
+                "configurationId": 1,
+                "key": "blah",
+                "createdAt": 1592849410863,
+                "updatedAt": 1592849410863,
+                "system": False,
+            },
+        )
+        # assert all the calls were made w/ the right args
+        # org = self.organization
+        # integration = Integration.objects.get(provider=self.provider.key)
+        # installation = integration.get_installation(org.id)
+        # data = {"project_mappings": [[1, "Qme9NXBpguaRxcXssZ1NWHVaM98MAL6PHDXUs1jPrgiM8H"],]}
+        # installation = integration.get_installation(self.organization)
+        # org_integration = OrganizationIntegration.objects.get(
+        #     organization_id=org.id, integration_id=integration.id
+        # )
+        # config = org_integration.config
+        # assert config == {}
+        # installation.update_organization_config(data)
+        # assert config == {
+        #     "project_mappings": [[2, "Qme9NXBpguaRxcXssZ1NWHVaM98MAL6PHDXUs1jPrgiM8H"],]
+        # }
+
+    @responses.activate
+    def test_update_org_config_vars_exist(self):
+        # test where the secret and env var already exist
+        pass
+
+    @responses.activate
+    def test_upgrade_org_config_no_dsn(self):
+        # test where there is no enabled DSN
+        pass
